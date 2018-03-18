9f54e18220fac44b5eca48c90acf3a10b719806c
Dave Stockdale <DStockdale@wagstaff.com>
2018-03-18 13:44:46 -0700

    heroku setup

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 
-app.use(express.static(__dirname))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
     //res.send('Hello world')
-    res.sendFile('./index.html')
-})
+    res.sendFile('./index.html');
+});
 
 
-app.listen(8080)
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
