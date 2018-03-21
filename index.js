67cda8035c411907a3f3baf1997c40b9de84e83b
Dave Stockdale <DStockdale@wagstaff.com>
2018-03-21 11:16:52 -0700

    changes and testing

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
-    //res.send('Hello world')
-    res.sendFile('./index.html');
+    res.send('Hello world ' + process.env.DAVES_NAME)
+    //res.sendFile('./index.html');
 });
 
 
