b0ec599616df7fbf905f1b93348cfbbfe4124bdb
Dave Stockdale <DStockdale@wagstaff.com>
2018-03-22 13:00:51 -0700

    setup..

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    res.send('Hello world ' + process.env.DAVES_NAME)
-    //res.sendFile('./index.html');
+    //res.send('Hello world ' + process.env.DAVES_NAME)
+    res.sendFile(__dirname + '/mainpage.html');
 });
 
 
